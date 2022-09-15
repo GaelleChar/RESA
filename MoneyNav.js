dfd7b3494b213826be7687855dcf0a77df95c800
Gaelle Charlet <milytulip9@gmail.com>
2022-09-15 15:48:43 -0400

    Delete MoneyNav.js

--- MoneyNav.js
+++
@@ -1,8 +0,0 @@
-/*
-<input type="home-page" onclick="location.href='MoneyHome.html';" value="Go to Home-page" />
-
-
-$('submit').on('click', function(){
-    window.location = "/Users/gaellecharlet/MoneyHome.html";    
-});
-*/
